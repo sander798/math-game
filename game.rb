6bbb67a7ca095fc9eb1b71e18adf7dcfaa7ff013
Alexander Evans <sander7989@gmail.com>
2023-11-25 20:28:06 -0700

    add comments

--- game.rb
+++ game.rb
@@ -22,9 +22,11 @@ class Game
     while player_1.lives > 0 && player_2.lives > 0
       puts "***** NEW TURN *****"
       
+      # Ask new question
       question = get_question
       puts "Player #{turn}: #{question[:text]}"
-      
+
+      # Get player answer
       player_answer = gets.chomp
 
       if player_answer == question[:answer].to_s
@@ -36,12 +38,14 @@ class Game
         puts "Nope!"
       end
 
+      # Score
       puts "Lives: P1: #{player_1.lives}/#{player_1.initial_lives} | P2: #{player_2.lives}/#{player_2.initial_lives}"
 
       turn = turn + 1
       turn = 1 if turn == 3
     end
 
+    # Game results
     if player_1.lives > 0
       puts "Player 1 wins! -> Score: #{player_1.lives}/#{player_1.initial_lives}"
     else
@@ -51,6 +55,7 @@ class Game
     puts "***** GAME OVER *****"
   end
 
+  # Generate question with answer together in a hash
   def get_question
     a = rand(1..20)
     b = rand(1..20)
